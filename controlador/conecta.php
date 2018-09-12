5cab54e1e2631381195161be1240f3f2858c23c7
HandyF <handyinfoaiep@gmail.com>
2018-09-11 22:36:42 -0300

    corregir cadena de conexion

--- controlador/conecta.php
+++ controlador/conecta.php
@@ -1,9 +1,19 @@
 <?php
+//$server   = "localhost";
+//$user     = "Handy";
+//$password = "12345";
+//$bd       = "gestionbodega1";
+
+//$conexion = mysqli_connect($server, $user, $password, $bd);
+//if (!$conexion) {
+//    die('Error de Conexión: ' . mysqli_connect_errno());
+//}
+
 $mysqli = new mysqli("localhost", "Handy", "12345", "gestionbodega1");
 
 /* comprobar la conexion */
 if ($mysqli->connect_errno) {
-    echo("Falló la conexión: %s/n". $mysqli->connect_error);
+    echo ("Falló la conexión: %s/n" . $mysqli->connect_error);
     exit();
 
 }
